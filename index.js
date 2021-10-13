c429387a345a17b69d817e19c726753e5abd79f2
raphael.aguiar <raphael@lifeapps.com.br>
2021-10-13 09:29:42 -0300

    teste build automatico

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/',(req,res) => {
-  res.status(200).send('Alive')
+  res.status(200).send('Alive 1.1')
 })
 
 app.listen(port);
