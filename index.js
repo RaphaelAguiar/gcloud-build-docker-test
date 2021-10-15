d0ba49eda2a0626cdc8ee8ede2dea3c05e4fd551
raphael.aguiar <raphael@lifeapps.com.br>
2021-10-15 09:54:51 -0300

    fix teste comando notificacao

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.status(200).send('Alive 1.1')
+})
+
+app.post('/teste', (req, res) => {
+  res.status(200).send([
+    {
+      name: 'notify',
+      args: {
+        username: 'puca-user',
+        message: 'Usuário alterado com sucesso'
+      }
+    }
+  ])
 })
 
 app.listen(port);
